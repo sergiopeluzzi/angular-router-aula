38232a25a1c6e6f3244cbbe43a4d861dcc8bf209
Sergio Peluzzi <semz@gft.com>
2023-11-10 11:34:14 -0300

    adicionado angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,16 @@ import { VeiculosCadastroComponent } from "./components/veiculos/veiculos-cadast
 import { VeiculosListagemComponent } from "./components/veiculos/veiculos-listagem/veiculos-listagem.component";
 import { FormsModule } from "@angular/forms";
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -25,7 +35,20 @@ import { FormsModule } from "@angular/forms";
         VeiculosCadastroComponent,
         VeiculosListagemComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatTableModule,
+        MatIconModule,
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/components/clientes/clientes-listagem/clientes-listagem.component.ts
+++ src/app/components/clientes/clientes-listagem/clientes-listagem.component.ts
@@ -1,10 +1,32 @@
 import { Component, Input } from "@angular/core";
 
+const clientes = [
+    {
+        id: 1,
+        nome: "João",
+        cpf: "12345678910",
+        telefone: "11912345678",
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        cpf: "12345678910",
+        telefone: "11912345678",
+    },
+    {
+        id: 3,
+        nome: "José",
+        cpf: "12345678910",
+        telefone: "11912345678",
+    },
+];
+
 @Component({
     selector: "app-clientes-listagem",
     templateUrl: "./clientes-listagem.component.html",
     styleUrls: ["./clientes-listagem.component.css"],
 })
 export class ClientesListagemComponent {
-    @Input() dados!: any[];
+    displayedColumns = ["id", "nome", "cpf", "telefone"];
+    dataSource = clientes;
 }
